726ade9754bf4d067291e72ba9c36821b75761ba
Matheus Albuquerque <land@ythecombinator.space>
2020-04-28 19:26:08 +0200

    chore(config): add constants

---
+++ src/config/constants.ts
@@ -0,0 +1,19 @@
+export const genres = [
+  { id: "axe", title: "Axé", featured: false },
+  { id: "dj", title: "DJ", featured: false },
+  { id: "forro", title: "Forró", featured: true },
+  { id: "funk", title: "Funk", featured: false },
+  { id: "gospel", title: "Gospel", featured: false },
+  { id: "mpb", title: "MPB", featured: false },
+  { id: "pop", title: "Pop", featured: false },
+  { id: "rap", title: "Rap", featured: false },
+  { id: "reggae", title: "Reggae", featured: false },
+  { id: "rock", title: "Rock", featured: false },
+  { id: "samba-pagode", title: "Samba/Pagode", featured: true },
+  { id: "sertanejo", title: "Sertanejo", featured: true },
+];
+
+export const endpoints = {
+  base: "https://lives.mus.br",
+  api: "https://api.lives.mus.br/lives",
+};
